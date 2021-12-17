73f45c95762879f97e9b344a35ea315751618caa
Tobi <65374030+tobikadachi937@users.noreply.github.com>
2021-12-17 14:52:15 -0600

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,27 +1,30 @@
 <?php
-$name = $_POST['name'];
-$mail = $_POST['mail'];
-$phone = $_POST['phone'];
-$message = $_POST['message']; 
+$remitente = $_POST['email'];
+$destinatario = 'zarakigrey@gmail.com'; // en esta lÃnea va el mail del destinatario.
+$asunto = 'Asunto de ejemplo'; // acÃ¡ se puede modificar el asunto del mail
+if (!$_POST){
+?>
 
-$header = 'From: ' .$mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . "\r\n"; 
-$header .= "Mime-Version: 1.0 \r\n"; 
-$header .= "Content-Type: text/plain"; 
+<?php
+}else{
+	 
+    $cuerpo = "Nombre y apellido: " . $_POST["nombre"] . "\r\n"; 
+    $cuerpo .= "TelÃ©fono: " . $_POST["telefono"] . "\r\n";
+    $cuerpo .= "Correo: " . $_POST["correo"] . "\r\n";
+    $cuerpo .= "Mensaje: " . $_POST["mensaje"] . "\r\n";
+    
+	//las lÃneas de arriba definen el contenido del mail. Las palabras que estÃ¡n dentro de $_POST[""] deben coincidir con el "name" de cada campo. 
+	// Si se agrega un campo al formulario, hay que agregarlo acÃ¡.
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
-$message .= "Su e-mail es: " . $mail . " \r\n"
-$message .= "Teléfono de contacto:  " . $phone . " \r\n";
-$message .= "Mensaje: " . $_POST['message'] . " \r\n";
-$message .= "Enviado el: " .date('d/m/Y', time());
+    $headers  = "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/plain; charset=utf-8\n";
+    $headers .= "X-Priority: 3\n";
+    $headers .= "X-MSMail-Priority: Normal\n";
+    $headers .= "X-Mailer: php\n";
+    $headers .= "From: \"".$_POST['nombre']." ".$_POST['apellido']."\" <".$remitente.">\n";
 
-$para = 'zarakigrey69@gmail.com';
-$asunto = 'Asunto del mensaje'; 
-
-mail($para, $asunto, utf8_decode($message), $header); 
-
-header("Location:index.html");
-
-
-
+    mail($destinatario, $asunto, $cuerpo, $headers);
+    
+    include 'index.html'; //se debe crear un html que confirma el envÃo
+}
 ?>
